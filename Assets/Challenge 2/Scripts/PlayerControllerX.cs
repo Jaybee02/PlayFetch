9b25a1324a29386a3f339b47f12ca9a1617925bb
Ogundipe Ajibike <ogundipeajibike002@gmail.com>
2022-09-23 11:57:53 +0100

    First Commit

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float slowDown = 0;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && slowDown <= 0)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            slowDown = 1;
+        }
+        if (slowDown > 0)
+        {
+            slowDown -= Time.deltaTime;
+        }
+    }
+}
